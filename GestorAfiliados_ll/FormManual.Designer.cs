2d8a77a20181c7bcb00d34647367d64e106801d3
Agusmat22 <agusgarcia569@gmail.com>
2023-12-06 16:28:40 -0300

    actualizado2

--- GestorAfiliados_ll/FormManual.Designer.cs
+++ GestorAfiliados_ll/FormManual.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormManual";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormManual));
+            lblConfiguracion = new Label();
+            btnManual = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)btnManual).BeginInit();
+            SuspendLayout();
+            // 
+            // lblConfiguracion
+            // 
+            lblConfiguracion.AutoSize = true;
+            lblConfiguracion.Font = new Font("Segoe UI Semibold", 19F, FontStyle.Bold, GraphicsUnit.Point);
+            lblConfiguracion.ForeColor = Color.FromArgb(13, 51, 70);
+            lblConfiguracion.Location = new Point(419, 61);
+            lblConfiguracion.Name = "lblConfiguracion";
+            lblConfiguracion.Size = new Size(104, 36);
+            lblConfiguracion.TabIndex = 13;
+            lblConfiguracion.Text = "Manual";
+            // 
+            // btnManual
+            // 
+            btnManual.Cursor = Cursors.Hand;
+            btnManual.Image = (Image)resources.GetObject("btnManual.Image");
+            btnManual.Location = new Point(341, 44);
+            btnManual.Name = "btnManual";
+            btnManual.Size = new Size(72, 68);
+            btnManual.SizeMode = PictureBoxSizeMode.Zoom;
+            btnManual.TabIndex = 15;
+            btnManual.TabStop = false;
+            // 
+            // FormManual
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(895, 527);
+            Controls.Add(btnManual);
+            Controls.Add(lblConfiguracion);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FormManual";
+            Text = "FormManual";
+            ((System.ComponentModel.ISupportInitialize)btnManual).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblConfiguracion;
+        private PictureBox btnManual;
     }
 }
